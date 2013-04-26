3837997465d4ffcbc8976cf227e0a37820cfbde3
Maximilian Tagher <feedback.tagher@gmail.com>
2013-04-26 11:51:14 -0700

    Initial Commit

---
+++ TestSDWebImageBug/MXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MXAppDelegate.h
+//  TestSDWebImageBug
+//
+//  Created by Maximilian Tagher on 4/26/13.
+//  Copyright (c) 2013 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSDWebImageBug/MXViewController.h
@@ -0,0 +1,13 @@
+//
+//  MXViewController.h
+//  TestSDWebImageBug
+//
+//  Created by Maximilian Tagher on 4/26/13.
+//  Copyright (c) 2013 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXViewController : UIViewController
+
+@end
